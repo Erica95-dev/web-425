52482abfe61fba3c1933ced5599c5955d306a185
Ericadp.developer <ericadp.developer@gmail.com>
2020-12-21 02:05:22 -0500

    Update person.interface.ts

--- week1/person.interface.ts
+++ week1/person.interface.ts
@@ -1,3 +1,11 @@
+ /* 
+Title: assignment 1.4
+Author: Erica Perry
+Date:12/19/2020
+Modified: Erica Perry
+Description: Typescript
+*/ 
+
 export interface IPerson {
     firstName: string;
     lastName: string;
