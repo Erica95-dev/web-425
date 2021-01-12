72d9f145c0d7c0bb02370071713a6689985a2790
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-12 02:25:55 -0500

    Update contact.component.spec.ts

--- week2/my-app/composer-app/src/app/contact/contact.component.spec.ts
+++ week2/my-app/composer-app/src/app/contact/contact.component.spec.ts
@@ -1,3 +1,10 @@
+/* 
+Title: assignment 2.4
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description: contact
+*/ 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
