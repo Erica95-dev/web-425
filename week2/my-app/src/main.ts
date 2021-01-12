44a19c8c0a0f697012526c903ff0352b5c333876
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-12 01:28:23 -0500

    Update main.ts

--- week2/my-app/src/main.ts
+++ week2/my-app/src/main.ts
@@ -1,3 +1,11 @@
+/* 
+Title: assignment 2.2
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description: 
+*/ 
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
