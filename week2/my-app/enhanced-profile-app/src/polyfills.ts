7e3108f537473ea02931fd5ed0cc8c09dc65ecc9
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-12 02:06:33 -0500

    Update polyfills.ts

--- week2/my-app/enhanced-profile-app/src/polyfills.ts
+++ week2/my-app/enhanced-profile-app/src/polyfills.ts
@@ -1,3 +1,11 @@
+/* 
+Title: assignment 2.3
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description: polyfills.ts
+*/ 
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
