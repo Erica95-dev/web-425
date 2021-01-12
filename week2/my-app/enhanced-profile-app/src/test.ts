1188d90079fb99bbd2d176f0843b6db9acefffee
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-12 02:07:29 -0500

    Update test.ts

--- week2/my-app/enhanced-profile-app/src/test.ts
+++ week2/my-app/enhanced-profile-app/src/test.ts
@@ -1,3 +1,11 @@
+/* 
+Title: assignment 2.3
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description:test.ts
+*/ 
+
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/zone-testing';
