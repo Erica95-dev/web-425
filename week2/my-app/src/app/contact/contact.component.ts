5e45ecec14ee1f717b24597101c6784d167e94d8
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-11 23:26:28 -0500

    Add files via upload

---
+++ week2/my-app/src/app/contact/contact.component.ts
@@ -0,0 +1,22 @@
+ /* 
+Title: assignment 2.2
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description: 
+*/
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
