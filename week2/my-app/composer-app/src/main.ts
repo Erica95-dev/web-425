0d2ee85823f573bae3a57aeab1ddd54f8f96c1fc
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-12 02:34:30 -0500

    Update main.ts

--- week2/my-app/composer-app/src/main.ts
+++ week2/my-app/composer-app/src/main.ts
@@ -1,3 +1,11 @@
+/* 
+Title: assignment 2.4
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description: main.ts
+*/ 
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
