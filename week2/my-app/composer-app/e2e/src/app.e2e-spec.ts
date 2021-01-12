ff1a3aaff169c9a7ff8ae1921d3637ede76eca2e
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-12 02:09:37 -0500

    Update app.e2e-spec.ts

--- week2/my-app/composer-app/e2e/src/app.e2e-spec.ts
+++ week2/my-app/composer-app/e2e/src/app.e2e-spec.ts
@@ -1,3 +1,10 @@
+/* 
+Title: assignment 2.4
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description: app.e2e-spec.ts
+*/ 
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
