647c351b27af8f6f30cdb9d8bce403b27ac84156
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-11 23:23:44 -0500

    Add files via upload

---
+++ week2/my-app/src/app/about/about.component.ts
@@ -0,0 +1,22 @@
+ /* 
+Title: assignment 2.2
+Author: Erica Perry
+Date:1/9/2021
+Modified: Erica Perry
+Description: 
+*/ 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
