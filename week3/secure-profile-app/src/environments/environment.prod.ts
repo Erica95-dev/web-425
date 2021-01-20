579a98155e6caf73999f8016a3fd81c4973a89b5
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 09:09:09 -0500

    Update environment.prod.ts

--- week3/secure-profile-app/src/environments/environment.prod.ts
+++ week3/secure-profile-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+/* 
+Title: assignment 3.4
+Author: Erica Perry
+Date:1/18/2021
+Modified: Erica Perry
+Description: app,routing.module.ts
+*/ 
+
 export const environment = {
   production: true
 };
