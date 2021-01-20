fa25fd5e1be8efcc858327f193c700c94724adc7
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 06:54:08 -0500

    Add files via upload

---
+++ week3/secure-profile-app/src/home/home.component.ts
@@ -0,0 +1,27 @@
+/**
+ * Title: home.component.ts
+ * Author: Erica Perry
+ * Date: 18 Jan 2021
+ * Description: Home component (Home page)
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  isLoggedIn: boolean;
+
+  constructor(private route: ActivatedRoute) {
+    this.isLoggedIn = Boolean(this.route.snapshot.queryParamMap.get('isLoggedIn'));
+  }
+
+  ngOnInit(): void {
+  }
+
+}
