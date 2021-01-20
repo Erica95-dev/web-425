dc37ad801d7536d4b1457cc2b283e88c5ba51633
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 10:26:53 -0500

    Update sign-in.guard.spec.ts

--- week3/secure-profile-app/enhanced-secure-profile-app/src/app/sign-in.guard.spec.ts
+++ week3/secure-profile-app/enhanced-secure-profile-app/src/app/sign-in.guard.spec.ts
@@ -1,3 +1,11 @@
+/*
+Title: assignment 3.4
+Author: Erica Perry
+Date:1/18/2021
+Modified: Erica Perry
+Description: image.html
+*/
+
 import { TestBed } from '@angular/core/testing';
 
 import { SignInGuard } from './sign-in.guard';
