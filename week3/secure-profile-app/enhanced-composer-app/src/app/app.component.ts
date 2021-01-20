b9ab6f1cf8c0722e57a2ed54b827b650af3841d9
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 08:44:45 -0500

    Create app.component.ts

---
+++ week3/secure-profile-app/enhanced-composer-app/src/app/app.component.ts
@@ -0,0 +1,8 @@
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment = 'Exercise 3.2 - Passing Data to Routes, Part 1';
+}
