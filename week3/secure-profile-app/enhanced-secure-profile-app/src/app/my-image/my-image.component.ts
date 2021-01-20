8b9c5f7543ba1a1a7a0fadfd1ff719cca836b3aa
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 10:31:25 -0500

    Update my-image.component.ts

--- week3/secure-profile-app/enhanced-secure-profile-app/src/app/my-image/my-image.component.ts
+++ week3/secure-profile-app/enhanced-secure-profile-app/src/app/my-image/my-image.component.ts
@@ -1,3 +1,11 @@
+/*
+Title: assignment 3.4
+Author: Erica Perry
+Date:1/18/2021
+Modified: Erica Perry
+Description: image.html
+*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
