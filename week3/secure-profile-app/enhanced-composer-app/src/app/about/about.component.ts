7c1c086a8c8e236a74571ee54df4e307171edcbb
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 09:41:22 -0500

    Update about.component.ts

--- week3/secure-profile-app/enhanced-composer-app/src/app/about/about.component.ts
+++ week3/secure-profile-app/enhanced-composer-app/src/app/about/about.component.ts
@@ -1,3 +1,11 @@
+/*
+Title: assignment 3.2
+Author: Erica Perry
+Date:1/18/2021
+Modified: Erica Perry
+Description: about.ts
+*/
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
