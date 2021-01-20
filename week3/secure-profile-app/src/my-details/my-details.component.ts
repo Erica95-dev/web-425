680e5e437543d53727867405481535bbca69e77a
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 06:56:22 -0500

    Add files via upload

---
+++ week3/secure-profile-app/src/my-details/my-details.component.ts
@@ -0,0 +1,41 @@
+/*
+Title: assignment 3.2
+Author: Erica Perry
+Date:1/8/2021
+Modified: Erica Perry
+Description: about.component
+*/
+import { Component, OnInit } from '@angular/core';
+
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    "#TypeScript", "#2020", "#CodingWithAngular", "#ngOmaha"
+  ]
+
+  constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName = fullName;
+    this.favoriteFood = favoriteFood;
+    this.favoriteColor = favoriteColor;
+  }
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  myProfile: Person;
+
+  constructor() {
+    this.myProfile = new Person("Erica", "Chicken", "Blue");
+  }
+
+  ngOnInit(): void {
+  }
+
+}
