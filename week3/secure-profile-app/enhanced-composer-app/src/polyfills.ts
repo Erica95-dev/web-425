529ee126d56a083b82b8238d3c9d4dad9a4ce2a5
Ericadp.developer <ericadp.developer@gmail.com>
2021-01-20 10:11:21 -0500

    Update polyfills.ts

--- week3/secure-profile-app/enhanced-composer-app/src/polyfills.ts
+++ week3/secure-profile-app/enhanced-composer-app/src/polyfills.ts
@@ -1,3 +1,11 @@
+/*
+Title: assignment 3.2
+Author: Erica Perry
+Date:1/18/2021
+Modified: Erica Perry
+Description: ts
+*/
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
