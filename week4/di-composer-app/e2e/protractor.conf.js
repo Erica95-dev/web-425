941ae15e556964e9de355a04d888a4f109c01396
Ericadp.developer <ericadp.developer@gmail.com>
2021-02-05 10:25:57 -0500

    Update protractor.conf.js

--- week4/di-composer-app/e2e/protractor.conf.js
+++ week4/di-composer-app/e2e/protractor.conf.js
@@ -1,3 +1,9 @@
+/**
+ * Title: protractor.conf.ts
+ * Author: Erica Perry
+ * Date: 21 Jan 2021
+ * Description: 
+ */
 // @ts-check
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
