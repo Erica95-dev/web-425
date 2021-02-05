6ac36d888c6b48cb1d11c372a191d11c93b7b536
Ericadp.developer <ericadp.developer@gmail.com>
2021-02-05 10:51:20 -0500

    Update about.component.ts

--- week4/reactive-composer-app/src/app/about/about.component.ts
+++ week4/reactive-composer-app/src/app/about/about.component.ts
@@ -1,3 +1,10 @@
+/*
+Title: about.component
+Author: Erica Perry
+Date:1/18/2021
+Modified: Erica Perry
+Description: about.component
+*/
 import { Component, OnInit } from '@angular/core';
 
 @Component({
