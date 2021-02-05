389797a2488579d1e6799dc389de3233e40aef4d
Ericadp.developer <ericadp.developer@gmail.com>
2021-02-05 10:33:14 -0500

    Update composer-details.component.ts

--- week4/enterprise-composer-app/src/app/composer-details/composer-details.component.ts
+++ week4/enterprise-composer-app/src/app/composer-details/composer-details.component.ts
@@ -1,7 +1,7 @@
 /**
  * Title: composer-details.component.ts
- * Author: Professor Krasso
- * Date: 21 April 2020
+ * Author: Erica Perry
+ * Date: 18 Jan 2021
  * Description: Composer details component (Composer details page)
  */
 
