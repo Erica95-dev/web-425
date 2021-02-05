0a2b163b3bf9b58a10265f8bd8b3c62aa68be823
Ericadp.developer <ericadp.developer@gmail.com>
2021-02-05 11:00:56 -0500

    Update contact.component.spec.ts

--- week4/reactive-composer-app/src/app/contact/contact.component.spec.ts
+++ week4/reactive-composer-app/src/app/contact/contact.component.spec.ts
@@ -1,3 +1,10 @@
+/*
+Title: conatct.component
+Author: Erica Perry
+Date:1/18/2021
+Modified: Erica Perry
+Description: 
+*/
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
